665679252414849c6c954020a209edd3a309dcd3
Paulo Henrique <paulohenriqueboger@gmail.com>
2023-06-30 00:53:55 -0300

    js quase completo

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando" />'
 const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado" />';
 const atividades = [];
 const notas = [];
+const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
+const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
+const notaMinima = parseFloat(prompt("Digite a Nota Mínima:"));
 
 let linhas = '';
 
@@ -18,13 +21,19 @@ function adicionaLinha() {
     const inputNomeAtividade = document.getElementById('nome-atividade');
     const inputNotaAtividade = document.getElementById('nota-atividade');
 
+    if (atividades.includes(inputNomeAtividade.value)) {
+        alert(`A atividade: ${inputNomeAtividade.value} já foi inserida`);
+        return;
+    } else {
+        
+    }
     atividades.push(inputNomeAtividade.value);
-    notas.push(inputNotaAtividade.value);
+    notas.push(parseFloat(inputNotaAtividade.value));
 
     let linha = '<tr>';
     linha += `<td>${inputNomeAtividade.value}</td>`;
     linha += `<td>${inputNotaAtividade.value}</td>`;
-    linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`;
+    linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`;
     linha += '</tr>';
 
     linhas += linha;
@@ -39,6 +48,18 @@ function atualizaTabela() {
 }
 
 function atualizaMediaFinal() {
-    console.log(atividades);
-    console.log(notas);
+    const mediaFinal = calculaNotaFinal();
+
+    document.getElementById('média-final-valor').innerHTML = mediaFinal;
+    document.getElementById('média-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
+}
+
+function calculaNotaFinal() {
+    let somaDasNotas = 0;
+
+    for (let i = 0; i < notas.length; i++) {
+        somaDasNotas += notas[i];
+    }
+
+    return somaDasNotas / notas.length;
 }
